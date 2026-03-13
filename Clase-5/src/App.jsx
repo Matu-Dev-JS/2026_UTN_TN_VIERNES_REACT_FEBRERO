710a94d0fdddff11181060c95616bd37e2eed35d
matu.dev <mati.a.gimenez2002@gmail.com>
2026-03-13 20:54:14 -0300

    Clase 5

--- Clase-5/src/App.jsx
+++ Clase-5/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ProductCard from './Components/ProductCard/ProductCard'
+import Counter from './Components/Counter/Counter'
+
 
 function App() {
   return (
@@ -7,10 +9,15 @@ function App() {
       <h1>My Page.com, <span>Hello World</span></h1>
       <h2>hola</h2>
       {/* Instanciar el componente product card */}
-      <ProductCard title={'TV Samsung 72"'} price={5000000}/>
-      <ProductCard title={'TV Samsung 50"'} price={2500000}/>
-      <ProductCard title={'TV Samsung 32"'} price={600000}/>
-      <ProductCard  title={'TV Samsung 92"'} price={7000000}/>
+      <ProductCard title={'TV Samsung 72"'} price={5000000} />
+      <ProductCard title={'TV Samsung 50"'} price={2500000} />
+      <ProductCard title={'TV Samsung 32"'} price={600000} />
+      <ProductCard title={'TV Samsung 92"'} price={7000000} />
+      <ProductCard title={'mesa grande'} price={500000} />
+
+      <Counter initial_value={0} />
+      <Counter initial_value={10} />
+      <Counter initial_value={20} />
     </div>
   )
 }
@@ -29,5 +36,15 @@ El html se vera asi:
 </div>
 
 Instanciar 3 Counter en el app.js
+
+La idea es tener 3 contadores independientes, osea cada uno con sus propios bontones
 */
 
+/* 
+Tarea para la clase 6:
+Hacer un componente llamado <Switch /> 
+que tendra un boton "Switch"
+Tendra un estado interno que puede ser "On" "Off", incialmente 'Off'
+Cuando des click a Switch el estado interno debe alternarse al estado opuesto
+Mostrar en un span el valor actual de tu estado interno <span>Estado actual: {internal_state}</span>
+*/
--- Clase-5/src/Components/ProductCard/ProductCard.jsx
+++ Clase-5/src/Components/ProductCard/ProductCard.jsx
@@ -3,14 +3,16 @@ Los componentes son funciones que retornan JSX (html)
 Los componentes son REUTILIZABLES
 props siempre es un objeto (incluso sino hay, en ese caso es {})
 */
-function ProductCard (props){
-  console.log(props)
+import './ProductCard.css'
+function ProductCard ({title, price}){
+
   return (
     <div>
-      <h2>{props.title}</h2>
-      <span>Precio: ${props.price}</span>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni fugiat eius debitis illo. Sapiente odit doloremque maxime nesciunt excepturi possimus eos eius sit nam natus, et officiis, ad magni vel.</p>
-      <button>Comprar</button>
+        <h2 className='product-card__title'>{title}</h2>
+        <span>Precio: ${price}</span>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni fugiat eius debitis illo. Sapiente odit doloremque maxime nesciunt excepturi possimus eos eius sit nam natus, et officiis, ad magni vel.</p>
+        <button>Comprar</button>
+        
     </div>
   )
 }
